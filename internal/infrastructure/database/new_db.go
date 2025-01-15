76299c9e0c99a3de4bdbce2d4e1094518eea0798
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-01-15 18:08:44 -0300

    feat: columns with fixed sizes

---
+++ internal/infrastructure/database/new_db.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"github.com/danubiobwm/goEmailN/internal/domain/campaign"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDb() *gorm.DB {
+	dsn := "host=localhost user=emailn_dev password=Danu1985 dbname=emailn_dev port=5432 sslmode=disable "
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&campaign.Campaign{}, &campaign.Contact{})
+	return db
+}
